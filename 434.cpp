6c0560d1c234b23aed53627ba596be8f59b36907
Hifang-xz <hixinzi5@gmail.com>
2026-05-06 18:25:20 +0800

    字符串中的单词数

---
+++ 434.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    int countSegments(string s)
+    {
+        int a = s.size();
+        int q = 0;
+        for (int i = 0; i < a; ++i)
+        {
+            if (s[i] != ' ' && (i == 0 || s[i - 1] == ' '))
+            {
+                ++q;
+            }
+        }
+        return q;
+    }
+};字符串中的单词数
