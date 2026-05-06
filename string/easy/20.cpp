b0589550c96cd321861ef0bb044e3bd2de18284c
Hifang-xz <hixinzi5@gmail.com>
2026-05-06 18:19:11 +0800

    有效的括号

--- string/easy/20.cpp
+++ string/easy/20.cpp
@@ -1,22 +1,31 @@
-class Solution {
+class Solution
+{
 public:
-    bool isValid(string s) {
-        if(s.size()%2!=0)return false;
-        stack<char> st;
-        for(char c:s){
-            if(c=='('||c=='['||c=='{'){
-            st.push(c);
-            }
-        else{if(st.empty())
+    bool isValid(string s)
+    {
+        if (s.size() % 2 != 0)
             return false;
-            char top = st.top();
-             st.pop();
-             if(c==')'&&top!='(')return false;
-             if(c==']'&&top!='[')return false;
-             if(c=='}'&&top!='{')return false;
+        stack<char> st;
+        for (char c : s)
+        {
+            if (c == '(' || c == '[' || c == '{')
+            {
+                st.push(c);
             }
-        
+            else
+            {
+                if (st.empty())
+                    return false;
+                char top = st.top();
+                st.pop();
+                if (c == ')' && top != '(')
+                    return false;
+                if (c == ']' && top != '[')
+                    return false;
+                if (c == '}' && top != '{')
+                    return false;
+            }
+        }
+        return st.empty();
     }
-    return st.empty();
-}
 };
